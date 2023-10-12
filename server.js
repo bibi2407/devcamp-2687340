4f199cdef381222beebce590a43c1acb08e21998
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2023-10-11 14:51:26 -1200

    servidor creado

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+//crear el objeto principal
+//de la api
+const app = express()
+
+//url de prueba
+app.get('/prueba' , function(request , response ){
+    response.send("Hola Bibi")
+})
+
+app.get('/prueba2' , (request , response)=> {
+    response
+        .status(200)
+        .json({
+            "success" : true
+        })
+})
+
+//crear el servidor de aplicacion:
+app.listen(5000 ,
+           console.log('servidor ejecutando en puerto ' + 5000 ))
